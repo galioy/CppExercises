11c3ab26f61b039137bf8a53667e199bc23de4be
Galin Kostov <gako@esmiley.dk>
2017-10-31 10:55:09 +0100

    Use print out the vector elements directly instead of using pointers, etc

--- ex0901.cpp
+++ ex0901.cpp
@@ -3,10 +3,17 @@
 
 using namespace std;
 
-void reverse(int *p, int lim) {
-    if (lim > 0) {
-        reverse(++p, --lim);
-        cout << *(p - 1) << ' ';
+//void reverse(int *p, int lim) {
+//    if (lim > 0) {
+//        reverse(++p, --lim);
+//        cout << *(p - 1) << ' ';
+//    }
+//}
+
+void reverseVector(vector<int> numbers, int idx) {
+    if (idx <= numbers.size()) {
+        cout << numbers[idx] << ' ';
+        reverseVector(numbers, idx - 1);
     }
 }
 
@@ -14,22 +21,23 @@ int main() {
     int n;
     vector<int> numbers;
 
-    for (int i = 1; i <= 10; i++) {
-        numbers.push_back(i);
-    }
-
-//    cin >> n;
-//    while(cin) {
-//        numbers.push_back(n);
-//        cin >> n;
+//    for (int i = 1; i <= 10; i++) {
+//        numbers.push_back(i);
 //    }
+
+    cin >> n;
+    while(cin) {
+        numbers.push_back(n);
+        cin >> n;
+    }
 
     int *p = new int[numbers.size()];
     for (int i = 0; i < numbers.size(); i++) {
         *(p + i) = numbers[i];
     }
 
-    reverse(p, numbers.size());
+//    reverse(p, numbers.size());
+    reverseVector(numbers, numbers.size() - 1);
 
     return 0;
 }
